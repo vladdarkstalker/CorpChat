0ffd8d19ade0c3720299309bc045a98e5f688475
vladdarkstalker <119122181+vladdarkstalker@users.noreply.github.com>
2024-07-07 16:06:19 +0300

    Revert "add simle log in/out"
    This reverts commit ab1f669a0313b31e3135c76a7b34bbb6ee9ef74f.

--- src/main/java/com/corpchat/CorpChat/repository/UserRepository.java
+++ src/main/java/com/corpchat/CorpChat/repository/UserRepository.java
@@ -6,5 +6,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
-    User findByEmail(String email);
+    List<User> findByFirstName(String firstName);
+    List<User> findByLastName(String lastName);
+    List<User> findByMiddleName(String middleName);
+    List<User> findByLogin(String login);
+    List<User> findByEmail(String email);
 }
