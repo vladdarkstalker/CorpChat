1a5a1d0152407f1ab2e163c9f668083de548c151
vladdarkstalker <119122181+vladdarkstalker@users.noreply.github.com>
2024-07-07 16:16:29 +0300

    Just clear smth

--- src/main/java/com/corpchat/CorpChat/service/UserService.java
+++ src/main/java/com/corpchat/CorpChat/service/UserService.java
@@ -13,43 +13,33 @@ public class UserService {
 
     @Autowired
     private UserRepository userRepository;
-
     public List<User> getAllUsers() {
         return userRepository.findAll();
     }
-
     public Optional<User> getUserById(Integer id) {
         return userRepository.findById(id);
     }
-
     public List<User> getUsersByLogin(String login) {
         return userRepository.findByLogin(login);
     }
-
     public List<User> getUsersByMiddleName(String middleName) {
         return userRepository.findByMiddleName(middleName);
     }
-
     public List<User> getUsersByLastName(String lastName) {
         return userRepository.findByLastName(lastName);
     }
-
     public List<User> getUsersByFirstName(String firstName) {
         return userRepository.findByFirstName(firstName);
     }
-
     public List<User> getUsersByEmail(String email) {
         return userRepository.findByEmail(email);
     }
-
     public User createUser(User user) {
         return userRepository.save(user);
     }
-
     public User updateUser(User user) {
         return userRepository.save(user);
     }
-
     public void deleteUser(Integer id) {
         userRepository.deleteById(id);
     }
